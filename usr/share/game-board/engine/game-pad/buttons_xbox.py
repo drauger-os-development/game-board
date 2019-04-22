5d6c07bbbf239eafc0578ffac1bb2301df990a0d
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-04-22 12:57:40 -0400

    new test situations
    * test situations added
    * button layout for Xbox 360 and Xbox One set up

--- usr/share/game-board/engine/game-pad/buttons_xbox.py
+++ usr/share/game-board/engine/game-pad/buttons_xbox.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  test.py
+#  buttons_xbox.py
 #  
 #  Copyright 2019 Thomas Castleman <contact@draugeros.org>
 #  
@@ -20,7 +20,9 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 #  
-# ''' 
+#  This script is for Xbox One and Xbox 360 controllers, specificly the buttons on it.
+#  The analog sticks are handled for all controllers in analog_in.py
+#
 import pygame
 import sys
 from subprocess import Popen
@@ -32,7 +34,7 @@ def set_procname(newname):
 	buff.value = newname                 #Null terminated string as it should be
 	libc.prctl(15, byref(buff), 0, 0, 0) #Refer to "#define" of "/usr/include/linux/prctl.h" for the misterious value 16 & arg[3..5] are zero as the man page says.
 
-set_procname("gb-tester")
+set_procname("gb-engine2")
 
 pygame.init()
 
@@ -50,59 +52,100 @@ try:
 						if button == 0:
 							print(button)
 							#button = "a"
+							Popen(["python","/usr/share/game-board/engine/conversion/type.py","space"])
 							break
 						elif button == 1:
 							print(button)
 							#button = "b"
+							try:
+								Popen(["/usr/bin/killall","gb-engine1"])
+	
+							except:
+								pass
+							
+							try:
+								Popen(["/usr/bin/killall","gb-gui"])
+	
+							except:
+								pass
+							
+							try:
+								Popen(["/usr/bin/killall","gb-engine2"])
+	
+							except:
+								pass
+								
+							exit(0)
 							break
 						elif button == 2:
 							#button = "x"
 							print(button)
+							Popen(["python","/usr/share/game-board/engine/conversion/type.py","enter"])
 							break
 						elif button == 3:
 							print(button)
 							#button = "y"
+							Popen(["python","/usr/share/game-board/engine/conversion/type.py","tab"])
 							break
 						elif button == 4:
 							print(button)
 							#button = "left_bumper"
+							Popen(["python","/usr/share/game-board/engine/conversion/type.py","left"])
 							break
 						elif button == 5:
 							print(button)
 							#button = "right_bumper"
+							Popen(["python","/usr/share/game-board/engine/conversion/type.py","right"])
+							''''''
 							break
 						elif button == 6:
 							print(button)
 							#button = "back"
+							try:
+								Popen(["/usr/bin/killall","gb-gui"])
+	
+							except:
+								#restart GUI
+								print("GUI not running!")
 							break
 						elif button == 7:
 							print(button)
 							#button = "start"
+							Popen(["/usr/share/game-board/engine/game-pad/sleep_mode.py"])
 							break
 						elif button == 8:
 							print(button)
 							#button = "menu""
+							Popen(["python","/usr/share/game-board/ui/config.py"])
 							break
 						elif button == 9:
 							print(button)
 							#left analog button
+							Popen(["python","/usr/share/game-board/engine/conversion/type.py","backspace"])
 							break
 						elif button == 10:
 							#right analog button
 							print(button)
-							break
-						elif button == 11:
-							print(button)
-							#button = None
-							break
-						elif button == 12:
-							print(button)
-							break
-						elif button == 13:
-							print(button)
+							stdin = sys.stdin
+							stdin = stdin.split()
+							char = stdin[0]
+							printed = 0
+							try:
+								float(char)
+								try:
+									int(char)
+									printed = 1
+								except:
+									printed = 0
+							except:
+								printed = 1
+							if printed == 1:
+								char = str(char)
+								Popen(["python","/usr/share/game-board/engine/conversion/type.py",char])
 							break
 						else:
 							print(button)
+							button = None
 							break
 					else:
 						button = button+1
@@ -110,4 +153,3 @@ try:
 except KeyboardInterrupt:
     print("EXITING NOW")
     j.quit()
-    
